fc543cc3a4cac044405d566f89911ca136eddc3c
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-01-26 16:04:24 +0800

    chore: for each

--- src/selftrain.cpp
+++ src/selftrain.cpp
@@ -4,14 +4,14 @@
 
 template<class RandomAccessIterator, typename Func>
 void
-mysort(RandomAccessIterator begin, RandomAccessIterator end, Func func)
+my_sort(RandomAccessIterator begin, RandomAccessIterator end, Func func)
 {
     using type = std::remove_cvref_t<decltype(*begin)>;
     static_assert(std::is_invocable_r_v<bool, Func, type, type>);
     while (true) {
         RandomAccessIterator beginBack = begin;
         RandomAccessIterator beginNext = begin + 1;
-        bool has_swap = false;
+        bool has_swap                  = false;
         while (beginNext != end) {
             if (func(*beginBack, *beginNext)) {
                 has_swap = true;
@@ -25,13 +25,31 @@ mysort(RandomAccessIterator begin, RandomAccessIterator end, Func func)
         }
     }
 }
-
+template<class RandomAccessIterator, typename Func>
+void
+my_for_each(RandomAccessIterator begin, RandomAccessIterator end, Func func)
+{
+    using type = std::remove_cv_t<decltype(*begin)>;
+    static_assert(std::is_invocable_r_v<void, Func, type>);
+    RandomAccessIterator beginBack = begin;
+    while (beginBack != end) {
+        func(*beginBack);
+        ++beginBack;
+    }
+}
 int
 main()
 {
     std::array<int, 7> a1{3, 1, 5, 8, 99, 88, -1};
 
-    mysort(a1.begin(), a1.end(), [](int a, int b) { return a > b; });
+    std::cout << "my sort" << std::endl;
+    my_sort(a1.begin(), a1.end(), [](int a, int b) { return a > b; });
+    for (auto i : a1) {
+        std::cout << i << std::endl;
+    }
+    my_for_each(a1.begin(), a1.end(), [](int &a) { a += 1; });
+
+    std::cout << "for each" << std::endl;
     for (auto i : a1) {
         std::cout << i << std::endl;
     }
